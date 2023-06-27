3b0babe7f4f3cd15c6203bc2c2ec3dcec270b722
zaryanezrya <ivan@sha.run>
2023-06-27 11:43:17 +0600

    Добавлен тест для сервиса 3

--- SalonLib.Test/ServiceLocatorTest.cs
+++ SalonLib.Test/ServiceLocatorTest.cs
@@ -10,11 +10,20 @@ public class ServiceLocatorTest
         Assert.Equal(expected, actual);
     }
 
-    [Fact]
+
+     [Fact]
     public void Service2Test()
     {
         var expected = "У рояля";
         var actual = SalonLib.ServiceLocator.GetService("Петь романсы");
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void Service3Test()
+    {
+        var expected = "В кабинете";
+        var actual = SalonLib.ServiceLocator.GetService("Писать статьи");
         Assert.Equal(expected, actual);
     }
 }
